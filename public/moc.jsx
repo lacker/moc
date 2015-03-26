42f8a0f307a1620463122e06473c8bfe705ca0fd
Kevin Lacker <lacker@gmail.com>
2015-03-26 16:13:08 -0700

    still hooking up the startery bits

--- public/moc.jsx
+++ public/moc.jsx
@@ -1,5 +1,13 @@
+// keys for moc.parseapp.com
+Parse.initialize(
+  "vaCDeEWI1qtZ0m95JFZttf3U7b4kLEQQWSAjWtV3",
+  "oLC9CG6BUtUwJGXiynv1M8ToKUpNmSMcB52GZwUS")
+
 
 $(document).ready(function() {
+
+
+  Parse.Cloud.run("scrapeHackerNews")
 
   console.log("called ready")
 
